0c117b029963c31868c67509bfba21549156db62
Rodrigo Peixoto <RODRIGOPEIXOTOT800@GMAIL.COM>
2023-06-30 21:54:45 -0300

    correção do erro de implantação da web

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function funcMenu() {
+    var itens = document.getElementById("itens")
+    var icon = document.getElementById("icone")
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+        
+
+    } else {
+        itens.style.display = 'block'
+    }
+}
