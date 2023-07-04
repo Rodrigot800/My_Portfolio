4f5e61ffa95e12fb255ebdb79dcb080585a68b87
Rodrigo Peixoto <RODRIGOPEIXOTOT800@GMAIL.COM>
2023-07-03 23:21:02 -0300

    1.1.7

--- script/script.js
+++ script/script.js
@@ -1,10 +1,24 @@
+ function carregar() {
+    var saudacao = document.getElementById('saudacao')
+    var dataAtual = new Date()
+    var hora = dataAtual.getHours()
+
+    if ( hora >= 6 && hora < 12) {
+        saudacao.innerHTML = `olá, bom dia!`
+    }
+    if (hora >= 12 && hora < 18) {
+        saudacao.innerHTML = `olá, boa tarde!`
+    }
+    if (hora >= 18 && hora < 6) {
+        
+    }
+}
+
 function funcMenu() {
     var itens = document.getElementById("itens")
     var icon = document.getElementById("icone")
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
-        
-
     } else {
         itens.style.display = 'block'
     }
